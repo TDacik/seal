535de41717b14eeb0ebd2c60ae988198fe34778e
Tomáš Dacík <idacik@fit.vut.cz>
2026-07-30 12:26:07 +0200

    Fix witness context registration into solver

--- src/validator/GlobalWitness.ml
+++ src/validator/GlobalWitness.ml
@@ -2,7 +2,12 @@ open CorrectnessWitness
 
 let witness = ref CorrectnessWitness.empty
 
-let set w = witness := w
+let register_predicates w =
+  List.iter Astral.GlobalSID.register_user_defined w.predicates
+
+let set w =
+  register_predicates w;
+  witness := w
 
 let get_loop_invariant stmt =
   let line = Common.stmt_line stmt in
